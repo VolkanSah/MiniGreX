020b1c3aa27659778b561e87a6b1576e96c9339b
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-24 03:10:37 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,4 @@
 <?php
-/*
-Hier wird das eingegebene Passwort mit password_hash() gehasht und dann mit einem Prepared Statement in die Datenbank eingefügt.
-Auch hier wird get_connection() verwendet, um eine sichere Verbindung zur Datenbank herzustellen.
-*/
 
 // Erforderliche Dateien einbinden
 require_once "config.php";
@@ -13,37 +9,75 @@ $conn = get_connection();
 
 // Formularabschicken überprüfen
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Benutzer registrieren
-  if (isset($_POST['username']) && isset($_POST['password'])) {
-    $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  
+  // Überprüfen, ob Benutzername bereits existiert
+  $stmt = $conn->prepare("SELECT id FROM users WHERE username = ?");
+  $stmt->bind_param("s", $username);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  if ($result->num_rows > 0) {
+    $error = "Benutzername bereits vergeben";
+  }
 
-    // Prepared Statement ausführen
-    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-    $stmt->bind_param("ss", $username, $password);
+  // Überprüfen, ob E-Mail-Adresse bereits existiert
+  $stmt = $conn->prepare("SELECT id FROM users WHERE email = ?");
+  $stmt->bind_param("s", $email);
+  $stmt->execute();
+  $result = $stmt->get_result();
+  if ($result->num_rows > 0) {
+    $error = "E-Mail-Adresse bereits registriert";
+  }
+
+  // Benutzer anlegen, wenn Benutzername und E-Mail-Adresse noch nicht existieren
+  if (!isset($error)) {
+    $hash = password_hash($password, PASSWORD_DEFAULT);
+    $stmt = $conn->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $username, $email, $hash);
     $stmt->execute();
+
+    // Weiterleitung zur Login-Seite
+    header("Location: login.php");
+    exit();
   }
 }
 
 // HTML-Kopf ausgeben
-echo "<!DOCTYPE html>
+$html = "<!DOCTYPE html>
 <html>
 <head>
   <title>Registrierung</title>
 </head>
 <body>
-  <h1>Registrierung</h1>
+  <h1>Registrierung</h1>";
+
+// Fehlermeldung ausgeben, falls vorhanden
+if (isset($error)) {
+  $html .= "<p style='color: red;'>{$error}</p>";
+}
+
+// Registrierungsformular ausgeben
+$html .= <<<HTML
   <form method='post'>
     <label for='username'>Benutzername:</label>
-    <input type='text' id='username' name='username'>
+    <input type='text' id='username' name='username' required>
+    <br>
+    <label for='email'>E-Mail-Adresse:</label>
+    <input type='email' id='email' name='email' required>
     <br>
     <label for='password'>Passwort:</label>
-    <input type='password' id='password' name='password'>
+    <input type='password' id='password' name='password' required>
     <br>
     <input type='submit' value='Registrieren'>
   </form>
-</body>
+HTML;
+
+// HTML-Fuß ausgeben
+$html .= "</body>
 </html>";
+print($html);
 
 // Verbindung zur Datenbank schließen
 $conn->close();
