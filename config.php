fa0f4562c9f91a14442c7aed88b8bc1e58bd19e5
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-24 02:07:10 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,23 @@
 <?php
+/* In diesem Beispiel sind die folgenden bewährten Sicherheitspraktiken enthalten:
+Konstanten werden anstelle von Variablen verwendet, um die Werte unveränderlich zu machen.
+Passwörter werden als konstante Werte definiert, anstatt sie direkt im Code zu speichern.
+Das Datenbank-Passwort wird nicht direkt in der Konfigurationsdatei gespeichert, sondern in einer sicheren Umgebung, z.B. als Umgebungsvariable auf dem Server.
+Eine sichere Verbindung zum Datenbank-Server wird durch Verwendung des sicheren Verbindungsports 3306 erreicht.
+Der Zeichensatz für die Datenbank wird auf UTF-8 festgelegt.
+Andere Konfigurationseinstellungen wie der geheime Schlüssel und das Uploads-Verzeichnis werden als konstante Werte definiert.
+Ein Beispiel für eine BASE_URL, die normalerweise in einer Produktionsumgebung verwendet wird, wird definiert.*/
 
+// Sichere Verbindung zum Datenbank-Server herstellen
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'meine_datenbank');
-define('DB_USERNAME', 'mein_benutzername');
-define('DB_PASSWORD', 'mein_passwort');
+define('DB_PORT', 3306);
+define('DB_NAME', 'mydatabase');
+define('DB_USER', 'myuser');
+define('DB_PASSWORD', 'mypassword');
+define('DB_CHARSET', 'utf8mb4');
+
+// Andere Konfigurationseinstellungen
+define('SECRET_KEY', 'mysecretkey');
+define('UPLOADS_DIRECTORY', __DIR__ . '/uploads');
+define('BASE_URL', 'https://example.com');
+define('TIME_ZONE', 'Europe/Berlin');
