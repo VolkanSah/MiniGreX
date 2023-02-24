d6cc9807916835d733b916259637b9a313e1456b
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-24 23:51:16 +0100

    Update home.php

--- themes/default/public/home.php
+++ themes/default/public/home.php
@@ -9,19 +9,22 @@ require_once "functions.php";
 // get database connection
 $conn = get_connection();
 
-// Load sitetitel and Meta-informationen
+// Load some stuff
 $site_info = get_site_info($conn);
-
-// HTML- in Variable 
-$html = "<main><h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>"; 
-$html = "<p class="fs-5 col-md-8">MiniGreX-CMS has been designed with security in mind, and the code has been written to minimize the risk of SQL 
-injection attacks and other security vulnerabilities. </p>";
-$html = "<div class="mb-5"><a href="hero-promo-home.php." class="btn btn-primary btn-lg px-4">Get involved</a></div>":
-$html = "  <hr class="col-3 col-md-2 mb-5">";
-$html = " <div class="row g-5">";
-$html = "  <div class="col-md-6">";
-$html = "  <h2>Starter projects</h2>";
+// site infos usedin this file
+// title, hero, homelink_title
 $posts = get_all_posts($conn);
+
+// HTML- in variable 
+// Hero Conatiner- in admin settings
+$html = "<main>;
+$html = "<h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>"; 
+$html = "<p class="fs-5 col-md-8">" . htmlspecialchars($site_info['hero'], ENT_QUOTES) . "</p>";
+$html = "<div class="mb-5"><a href="hero-promo-home.php." class="btn btn-primary btn-lg px-4">Get involved</a></div>":
+$html = "<hr class="col-3 col-md-2 mb-5">";
+$html = "<div class="row g-5">";
+$html = "<div class="col-md-6">";
+$html = "<h2>Starter projects</h2>";
 // start loop
 foreach ($posts as $post) {
   $comments = get_comments_for_post($conn, $post['id']);
@@ -31,7 +34,8 @@ foreach ($posts as $post) {
     $username = get_username($conn, $comment['benutzer_id']);
     $html .= "<p>" . htmlspecialchars($comment['text'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</p>";
   }
-  // if logged in sgow comments
+  //end loop
+  // if logged in show comments
   if (is_logged_in()) {
     $html .= "<form method='post' action='add_comment.php'>
       <input type='hidden' name='post_id' value='" . htmlspecialchars($post['id'], ENT_QUOTES) . "'>
@@ -40,31 +44,29 @@ foreach ($posts as $post) {
       <br>
       <input type='submit' value='Absenden'>
     </form>";
-  }
+  } // end show comment
 }
-
-$html = "    </div>  </div>";
-$html = "";
-$html = "";
-$html = "";
-
+$html = "    </div>  
+$html = "</div>";
+$html = "<div class="col-md-6">";
+$html = "<h2>" . htmlspecialchars($site_info['homelink_title'], ENT_QUOTES) . "</h2>";
+$html = "<p>Read more detailed instructions and documentation on using or contributing to Bootstrap.</p>";
+$html = "<ul class="icon-list ps-0">";
+$html = "<li class="d-flex align-items-start mb-1"><a href="../getting-started/introduction/">Privacy & Inprint</a></li>";
+$html = "<li class="d-flex align-items-start mb-1"><a href="../getting-started/webpack/">Donate for MiniGgreX</a></li>";
+$html = "<li class="d-flex align-items-start mb-1"><a href="../getting-started/parcel/">MiniGreX on Github</a></li>";
+$html = "<li class="d-flex align-items-start mb-1"><a href="../getting-started/vite/">Documantation of MiniGreX</a></li>";
+$html = " <li class="d-flex align-items-start mb-1"><a href="../getting-started/contribute/">Contributing to MiniGreX</a></li>";
+$html = "</ul>";
+$html = "</div>";
+$html = "</div>";
+$html = "</main>";
   
+  
+  
+  // HTML-Output 
+print($html);
 
-      <div class="col-md-6">
-        <h2>Guides</h2>
-        <p>Read more detailed instructions and documentation on using or contributing to Bootstrap.</p>
-        <ul class="icon-list ps-0">
-          <li class="d-flex align-items-start mb-1"><a href="#">link</a></li>
-           <li class="d-flex align-items-start mb-1"><a href="#">link</a></li>
-           <li class="d-flex align-items-start mb-1"><a href="#">link</a></li>
-           <li class="d-flex align-items-start mb-1"><a href="#">link</a></li>
-           <li class="d-flex align-items-start mb-1"><a href="#">link</a></li>
-          
-     
-        </ul>
-      </div>
-      <!-- end section for Edit to your needs -->
-
-    </div>
-  </main> "; 
+// close databse connection
+$conn->close();
 
