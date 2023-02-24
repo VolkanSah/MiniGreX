ccd747c0223019e5667ad14b0b10286c93f26ec0
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-24 02:28:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+/*
+In diesem Beispiel wird das prepare()-Methode von PDO verwendet, um ein Prepared Statement zu erstellen. 
+Das Statement mit den erforderlichen Parametern wird ausgeführt, indem execute()-Methode aufgerufen wird.
+
+*/
 
 // Erforderliche Dateien einbinden
 require_once "config.php";
@@ -7,45 +12,35 @@ require_once "functions.php";
 // Verbindung zur Datenbank herstellen
 $conn = get_connection();
 
-// Seitentitel und Meta-Informationen laden
-$site_info = get_site_info();
+// Komplette Liste von Einträgen aus der Datenbank holen
+$stmt = $conn->prepare("SELECT * FROM entries ORDER BY id DESC");
+$stmt->execute();
+$result = $stmt->get_result();
 
 // HTML-Kopf ausgeben
 echo "<!DOCTYPE html>
 <html>
 <head>
-  <title>{$site_info['title']}</title>
-  <meta name='description' content='{$site_info['description']}'>
+  <title>MiniGreX</title>
 </head>
 <body>
-  <h1>{$site_info['title']}</h1>";
+  <h1>MiniGreX</h1>
+  <form method='post'>
+    <input type='text' name='url' placeholder='URL eingeben'>
+    <input type='submit' value='Teilen'>
+  </form>
+  <h2>Neueste Einträge</h2>
+  <ul>";
 
-// Alle Beiträge aus der Datenbank laden
-$posts = get_all_posts();
-
-// Alle Beiträge anzeigen
-foreach ($posts as $post) {
-  $comments = get_comments_for_post($post['id']);
-  $username = get_username($post['benutzer_id']);
-  echo "<h2>{$post['link']} von {$username}</h2>";
-  foreach ($comments as $comment) {
-    $username = get_username($comment['benutzer_id']);
-    echo "<p>{$comment['text']} von {$username}</p>";
-  }
-  if (is_logged_in()) {
-    echo "<form method='post' action='add_comment.php'>
-      <input type='hidden' name='post_id' value='{$post['id']}'>
-      <label for='text'>Kommentar hinzufügen:</label>
-      <input type='text' id='text' name='text'>
-      <br>
-      <input type='submit' value='Absenden'>
-    </form>";
-  }
+// Neueste Einträge anzeigen
+while ($row = $result->fetch_assoc()) {
+  echo "<li><a href='" . htmlspecialchars($row['url']) . "' target='_blank'>" . htmlspecialchars($row['title']) . "</a></li>";
 }
+
+// HTML-Fuss ausgeben
+echo "</ul>
+</body>
+</html>";
 
 // Verbindung zur Datenbank schließen
 $conn->close();
-
-// HTML-Fuß ausgeben
-echo "</body>
-</html>";
