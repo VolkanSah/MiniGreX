d0c8c2a0a963c13d8c6b4ff6446e96b83535ecc8
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-24 21:44:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -10,15 +10,6 @@ require_once "functions.php";
 // get database connection
 $conn = get_connection();
 
-// Load some stuff
-$seo_manager = get_site_seo_plugin($conn);
-$image_db = get_site_image_plugin($conn);
-$video_db = get_site_video_plugin($conn);
-$auto_cache = get_site_cache_plugin($conn);
-$role_manager = get_site_role_plugin($conn);
-$profile_manager = get_site_profile_plugin($conn);
-$get_local_settings = get_site_local_plugin($conn);
-$get_gateway_manager = get_site_gateway_plugin($conn);
 // Load sitetitel and Meta-informationen
 $site_info = get_site_info($conn);
 
