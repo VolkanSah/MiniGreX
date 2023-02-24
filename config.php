8f9d76828ee5039065c866fd803b7b292a7dd705
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-24 02:49:02 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,26 @@
 <?php
-/* In diesem Beispiel sind die folgenden bewährten Sicherheitspraktiken enthalten:
-Konstanten werden anstelle von Variablen verwendet, um die Werte unveränderlich zu machen.
-Passwörter werden als konstante Werte definiert, anstatt sie direkt im Code zu speichern.
-Das Datenbank-Passwort wird nicht direkt in der Konfigurationsdatei gespeichert, sondern in einer sicheren Umgebung, z.B. als Umgebungsvariable auf dem Server.
-Eine sichere Verbindung zum Datenbank-Server wird durch Verwendung des sicheren Verbindungsports 3306 erreicht.
-Der Zeichensatz für die Datenbank wird auf UTF-8 festgelegt.
-Andere Konfigurationseinstellungen wie der geheime Schlüssel und das Uploads-Verzeichnis werden als konstante Werte definiert.
-Ein Beispiel für eine BASE_URL, die normalerweise in einer Produktionsumgebung verwendet wird, wird definiert.*/
+/**
+Die Zugangsdaten zur Datenbank werden hier als Konstanten definiert, um zu verhindern, dass sie aus Versehen überschrieben werden. 
+Außerdem werden die session.cookie_httponly und session.cookie_secure Einstellungen gesetzt, um Cookies sicherer zu machen.
+Zuletzt wird überprüft, ob die Seite über HTTPS aufgerufen wird, und gegebenenfalls eine Umleitung auf HTTPS durchgeführt.
 
-// Sichere Verbindung zum Datenbank-Server herstellen
+**/
+
+// Datenbank-Zugangsdaten
 define('DB_HOST', 'localhost');
-define('DB_PORT', 3306);
-define('DB_NAME', 'mydatabase');
-define('DB_USER', 'myuser');
-define('DB_PASSWORD', 'mypassword');
-define('DB_CHARSET', 'utf8mb4');
+define('DB_NAME', 'meine_db');
+define('DB_USER', 'mein_user');
+define('DB_PASS', 'mein_sicheres_passwort');
 
-// Andere Konfigurationseinstellungen
-define('SECRET_KEY', 'mysecretkey');
-define('UPLOADS_DIRECTORY', __DIR__ . '/uploads');
-define('BASE_URL', 'https://example.com');
-define('TIME_ZONE', 'Europe/Berlin');
+// Sitzungsname
+define('SESSION_NAME', 'meine_sitzung');
+
+// Cookies sicherer machen
+ini_set('session.cookie_httponly', 1);
+ini_set('session.cookie_secure', 1);
+
+// SSL erzwingen
+if ($_SERVER['HTTPS'] !== 'on') {
+    header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+    exit();
+}
