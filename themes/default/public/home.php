ac5fb52abf095e2d2349f6dac861866ad0823041
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 16:45:44 +0100

    Update home.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- themes/default/public/home.php
+++ themes/default/public/home.php
@@ -25,26 +25,16 @@ $html = "<hr class="col-3 col-md-2 mb-5">";
 $html = "<div class="row g-5">";
 $html = "<div class="col-md-6">";
 $html = "<h2>Starter projects</h2>";
-// start loop
-foreach ($posts as $post) {
-  $comments = get_comments_for_post($conn, $post['id']);
-  $username = get_username($conn, $post['benutzer_id']);
-  $html .= "<h2>" . htmlspecialchars($post['link'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</h2>";
-  foreach ($comments as $comment) {
-    $username = get_username($conn, $comment['benutzer_id']);
-    $html .= "<p>" . htmlspecialchars($comment['text'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</p>";
-  }
-  //end loop
-  // if logged in show comments
-  if (is_logged_in()) {
-    $html .= "<form method='post' action='add_comment.php'>
-      <input type='hidden' name='post_id' value='" . htmlspecialchars($post['id'], ENT_QUOTES) . "'>
-      <label for='text'>Kommentar hinzufügen:</label>
-      <input type='text' id='text' name='text'>
-      <br>
-      <input type='submit' value='Absenden'>
-    </form>";
-  } // end show comment
+// start loop here
+
+
+ // ??? was nutzen wir?
+
+
+
+
+  //end loop here
+
 }
 $html = "    </div>  
 $html = "</div>";
