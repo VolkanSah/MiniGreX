c82f7f37ac367e6f1093f2762cfc10059e863375
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 21:35:38 +0100

    Update user_cp.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- panel/user_cp.php
+++ panel/user_cp.php
@@ -1,15 +1,20 @@
 <?php
+// check if is admin
+if (!is_admin()) {
+  header('Location: index.php');
+  exit();
+}
 
 // load init.php
 require_once "init.php";
 
 // Verbindung zur Datenbank herstellen
-//$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Verbindung prüfen
-//if ($conn->connect_error) {
-//    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
-//}
+if ($conn->connect_error) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+}
 
 // Benutzer-ID aus der URL holen
 $benutzer_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
