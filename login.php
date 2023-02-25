7433626216c795514cfaddcf7b80a4b4074493cf
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 21:26:34 +0100

    Update login.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- login.php
+++ login.php
@@ -1,36 +1,35 @@
 <?php
-require_once "config.php";
-require_once "functions.php";
-
 // Wenn bereits angemeldet, dann weiterleiten zur Index-Seite
 if (is_logged_in()) {
   header('Location: index.php');
   exit();
 }
+// load init.php
+require_once "init.php";
 
 // Verbindung zur Datenbank herstellen
 $conn = get_connection();
 
-// Formularabschicken überprüfen
+// check formular sending
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = mysqli_real_escape_string($conn, $_POST['username']);
   $password = mysqli_real_escape_string($conn, $_POST['password']);
 
-  // Benutzerdaten abrufen
+  // call userdata with stmt only
   $stmt = $conn->prepare("SELECT id, password FROM users WHERE username = ?");
   $stmt->bind_param("s", $username);
   $stmt->execute();
   $result = $stmt->get_result();
   $row = $result->fetch_assoc();
 
-  // Passwort überprüfen
+  // check password
   if ($row && password_verify($password, $row['password'])) {
-    // Anmeldung erfolgreich, Session starten
+    // login allowed, Session start
     session_start();
     $_SESSION['user_id'] = $row['id'];
     $_SESSION['user_role'] = 'user';
 
-    // Weiterleiten zur Index-Seite
+    // redirekct to Index
     header('Location: index.php');
     exit();
   } else {
@@ -39,35 +38,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 
 // HTML-Kopf ausgeben
-$html = "<!DOCTYPE html>
-<html>
-<head>
-  <title>Anmeldung</title>
-</head>
-<body>
-  <h1>Anmeldung</h1>";
+$html = "
+  <h1>Login</h1>";
 
 // Fehlermeldung ausgeben, falls vorhanden
 if (isset($error_message)) {
   $html .= "<p>{$error_message}</p>";
 }
 
-// Anmeldeformular ausgeben
+// output login formular
 $html .= "<form method='post'>
-    <label for='username'>Benutzername:</label>
+    <label for='username'>Username:</label>
     <input type='text' id='username' name='username'>
     <br>
-    <label for='password'>Passwort:</label>
+    <label for='password'>Password:</label>
     <input type='password' id='password' name='password'>
     <br>
-    <input type='submit' value='Anmelden'>
+    <input type='submit' value='Login'>
   </form>";
 
-// HTML-Fuß ausgeben
-$html .= "</body>
-</html>";
-
-// HTML-Output ausgeben
+// HTML-Output 
 echo $html;
 
 // Verbindung zur Datenbank schließen
