033f9a332dfa2ce6506f173e747b1863ffdf283e
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 18:22:30 +0100

    Update user_cp.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- user_cp.php
+++ user_cp.php
@@ -1,6 +1,5 @@
 <?php
-require_once "config.php";
-require_once "functions.php";
+require_once "init.php";
 
 // Verbindung zur Datenbank herstellen
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
