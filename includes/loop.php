c61a496de2749943f427f55f3b82592cb6804321
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 17:12:40 +0100

    Create loop.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

---
+++ includes/loop.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This is the loop file, so it is more simple to edit it or call 
+ * This file is used to markup the loop aspects of the MiniGreX.
+ *
+ * @link       https://github.com/VolkanSah/MiniGreX-CMS/blob/main/includes/loop.php
+ * @since      1.0.0
+ *
+ * @package    MiniGreX 1.0 Loop
+ * @subpackage Wis_Business_Directory/public/template
+ */
+// load required files
+require_once "config.php";
+require_once "functions.php";
+require_once "plugins/plugin-loader.php";
+require_once "includes/upload.php";
+// get database connection
+$conn = get_connection();
+// Load sitetitel and Meta-informationen
+$site_info = get_site_info($conn);
+// Alle Beiträge aus der Datenbank laden
+$posts = get_all_posts($conn);
+// Alle Beiträge anzeigen
+foreach ($posts as $post) {
+  $comments = get_comments_for_post($conn, $post['id']);
+  $username = get_username($conn, $post['benutzer_id']);
+  $html .= "<h2>" . htmlspecialchars($post['link'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</h2>";
+  foreach ($comments as $comment) {
+    $username = get_username($conn, $comment['benutzer_id']);
+    $html .= "<p>" . htmlspecialchars($comment['text'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</p>";
+  }
+  if (is_logged_in()) {
+    $html .= "<form method='post' action='add_comment.php'>
+      <input type='hidden' name='post_id' value='" . htmlspecialchars($post['id'], ENT_QUOTES) . "'>
+      <label for='text'>Comment:</label>
+      <input type='text' id='text' name='text'>
+      <br>
+      <input type='submit' value='Absenden'>
+    </form>";
+  }
+}
+
+// HTML-Fuß in Variable schreiben
+$html .= "</body>
+</html>";
+
+// HTML-Output ausgeben
+print($html);
+
+// Verbindung zur Datenbank schließen
+$conn->close();
