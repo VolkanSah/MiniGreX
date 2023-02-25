19b30dd004b3e8f9360b0723924f56e7b113b2a4
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 19:52:45 +0100

    Update footer.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- themes/default/public/footer.php
+++ themes/default/public/footer.php
@@ -1,30 +1,21 @@
 <?php
-/* 
-MiniGreX 1.0
-*/
-
-// load required files
-require_once "config.php";
-require_once "functions.php";
-
+/* MiniGreX 1.0 /themes/default/public/footer.php */
+// load init.php
+require_once "init.php";
 // get database connection
 $conn = get_connection();
-
 // Load sitetitel and Meta-informationen
-$site_info = get_site_info($conn);  
-
-$html .= "<footer class="pt-5 my-5 text-muted border-top">";
-$html .= " Created with MiniGreX &copy; 2023";
-$html .= " </footer>";
-$html .= "</div>";
-$html .= "    <script src="https://cdn.com/bootstrap.bundle.min.js"></script>";
-    
+$site_info = get_site_info($conn); 
+// create footer
+$html = "<footer class="pt-5 my-5 text-muted border-top">";
+$html = " Created with MiniGreX &copy; 2023";
+$html = " </footer>";
+$html = "</div>";
+$html = "<script src="https://cdn.com/bootstrap.bundle.min.js"></script>";    
 // HTML-Fuß in Variable schreiben
 $html .= "</body>
 </html>";
-
 // HTML-Output ausgeben
 print($html);
-
 // Verbindung zur Datenbank schließen
 $conn->close();
