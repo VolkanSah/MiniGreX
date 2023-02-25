b2143717895a7c35709f41c9da4712fcb187cf8d
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 18:24:22 +0100

    Update index.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- index.php
+++ index.php
@@ -2,63 +2,8 @@
 /* 
 MiniGreX 1.0
 */
+require_once "init.php";
 
-// load required files
-require_once "config.php";
-require_once "functions.php";
-require_once "plugins/plugin-loader.php";
-require_once "includes/upload.php";
-
-// get database connection
-$conn = get_connection();
-
-// Load sitetitel and Meta-informationen
-$site_info = get_site_info($conn);
-
-
-
-
-
-
-// HTML-Kopf in Variable 
-$html = "<!DOCTYPE html>
-<html>
-<head>
-  <title>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</title>
-  <meta name='description' content='" . htmlspecialchars($site_info['description'], ENT_QUOTES) . "'>
-</head>
-<body>
-  <h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>";
-
-// Alle Beiträge aus der Datenbank laden
-$posts = get_all_posts($conn);
-
-// Alle Beiträge anzeigen
-foreach ($posts as $post) {
-  $comments = get_comments_for_post($conn, $post['id']);
-  $username = get_username($conn, $post['benutzer_id']);
-  $html .= "<h2>" . htmlspecialchars($post['link'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</h2>";
-  foreach ($comments as $comment) {
-    $username = get_username($conn, $comment['benutzer_id']);
-    $html .= "<p>" . htmlspecialchars($comment['text'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</p>";
-  }
-  if (is_logged_in()) {
-    $html .= "<form method='post' action='add_comment.php'>
-      <input type='hidden' name='post_id' value='" . htmlspecialchars($post['id'], ENT_QUOTES) . "'>
-      <label for='text'>Kommentar hinzufügen:</label>
-      <input type='text' id='text' name='text'>
-      <br>
-      <input type='submit' value='Absenden'>
-    </form>";
-  }
-}
-
-// HTML-Fuß in Variable schreiben
-$html .= "</body>
-</html>";
-
-// HTML-Output ausgeben
-print($html);
 
 // Verbindung zur Datenbank schließen
 $conn->close();
