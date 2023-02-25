9793e9fe2cbc6a396728abc77ed0cdfa0005fbd2
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-02-25 18:49:11 +0100

    Update init.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- init.php
+++ init.php
@@ -23,7 +23,7 @@ try {
 // Define constants for DB tables
 define("USERS_TABLE", "users");
 define("POSTS_TABLE", "posts");
-//define("POSTS_TABLE", "sites");
+define("POSTS_TABLE", "sites");
 define("COMMENTS_TABLE", "comments");
 
 // if exist define
@@ -36,14 +36,23 @@ define("COMMENTS_TABLE", "comments");
 
 
 // Prepare statements for commonly used queries
+// User prepared statements
 $stmt_select_all_users = $pdo->prepare("SELECT * FROM " . USERS_TABLE);
 $stmt_select_user_by_id = $pdo->prepare("SELECT * FROM " . USERS_TABLE . " WHERE id = :id");
 $stmt_insert_user = $pdo->prepare("INSERT INTO " . USERS_TABLE . " (username, password, email) VALUES (:username, :password, :email)");
-
+// Posts prepared statements
 $stmt_select_all_posts = $pdo->prepare("SELECT * FROM " . POSTS_TABLE);
 $stmt_select_post_by_id = $pdo->prepare("SELECT * FROM " . POSTS_TABLE . " WHERE id = :id");
 $stmt_insert_post = $pdo->prepare("INSERT INTO " . POSTS_TABLE . " (title, content, author_id, category) VALUES (:title, :content, :author_id, :category)");
-
+// Comments prepared statements
 $stmt_select_comments_by_post_id = $pdo->prepare("SELECT * FROM " . COMMENTS_TABLE . " WHERE post_id = :post_id");
 $stmt_insert_comment = $pdo->prepare("INSERT INTO " . COMMENTS_TABLE . " (post_id, author_name, content) VALUES (:post_id, :author_name, :content)");
+// Sites with category and  prepared statements
+$stmt_select_all_sites = $pdo->prepare("SELECT * FROM " . SITES_TABLE);
+$stmt_select_sites_by_id = $pdo->prepare("SELECT * FROM " . SITES_TABLE . " WHERE id = :id");
+$stmt_insert_sites = $pdo->prepare("INSERT INTO " . SITES_TABLE . " (title, content, author_id, category) VALUES (:title, :content, :author_id, :category)");
+// DEMO Dummy for optional Plugins like image_db (image Database & Hosting)
+$stmt_select_all_image_dbs = $pdo->prepare("SELECT * FROM " . IMAGE_DB_TABLE);
+$stmt_select_image_dbs_by_id = $pdo->prepare("SELECT * FROM " . IMAGE_DB_TABLE . " WHERE id = :id");
+$stmt_insert_image_dbs = $pdo->prepare("INSERT INTO " . IMAGE_DB_TABLE . " (title, content, link, author_id, category) VALUES (:title, :content, :link, :author_id, :category)");
 
