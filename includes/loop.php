b99489e84f0e9ec2a9853f74c58442da64c54c34
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-01 00:11:46 +0100

    Update loop.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/loop.php
+++ includes/loop.php
@@ -9,8 +9,14 @@
  */
 // load init.php
 require_once "init.php";
-// get database connection
-$conn = get_connection();
+// Verbindung zur Datenbank herstellen
+function get_connection() {
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+ if ($conn->connect_error) {
+  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+ }
+ return $conn;
+}
 // Load sitetitel and Meta-informationen
 $site_info = get_site_info($conn);
 // Start Loop
