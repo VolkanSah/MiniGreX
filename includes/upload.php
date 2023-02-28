77ed04106f063aa8c4675a8989a7dd7b0141b42f
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-01 00:12:27 +0100

    Update upload.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/upload.php
+++ includes/upload.php
@@ -9,6 +9,14 @@
 <?php
 // load init.php
 require_once "init.php";
+// Verbindung zur Datenbank herstellen
+function get_connection() {
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+ if ($conn->connect_error) {
+  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+ }
+ return $conn;
+}
 
 // Get reference to uploaded image
 $image_file = $_FILES["image"];
