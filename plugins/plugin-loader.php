8ae1eea8589ff5f86cd6cadb0e64deb45aafbce0
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-01 01:15:06 +0100

    Update plugin-loader.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- plugins/plugin-loader.php
+++ plugins/plugin-loader.php
@@ -1,32 +1,30 @@
 <?php
-/* 
-MiniGreX 1.0 Plugin Loader with stmt & pod
-*/
+/*  MiniGreX 1.0 Plugin Loader with stmt & pod not finished !*/
 // load init.php
 require_once "init.php";
 
-// load your plugin_core here
+// Verbindung zur Datenbank herstellen
+function get_connection() {
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+ if ($conn->connect_error) {
+  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+ }
+ return $conn;
+}
+
+// load your plugin_core here for init
 require_once "role_manager/core.php";
-require_once "auto_cache/core.php";
 require_once "gateway_manager/core.php";
 require_once "get_local_settings/core.php";
 require_once "image_db/core.php";
 require_once "profile_manager/core.php";
-require_once "seo_manager/core.php";
 require_once "video_db/core.php";
 require_once "role_manager/core.php";
-require_once "role_manager/core.php";
-
-
-
-// get database connection
-$conn = get_connection();
 
 // Load plugins 
 $seo_manager = get_site_seo_plugin($conn);
 $image_db = get_site_image_plugin($conn);
 $video_db = get_site_video_plugin($conn);
-$auto_cache = get_site_cache_plugin($conn);
 $role_manager = get_site_role_plugin($conn);
 $profile_manager = get_site_profile_plugin($conn);
 $get_local_settings = get_site_local_plugin($conn);
