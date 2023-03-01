1caf4621a0cab62ca60b008eed0f8e46be75d2a4
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-01 01:07:33 +0100

    Update option_settings.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- panel/option_settings.php
+++ panel/option_settings.php
@@ -1,16 +1,12 @@
 <?php
-// check if is admin
-if (!is_admin()) {
-  header('Location: index.php');
-  exit();
-}
 // load init.php
 require_once "init.php";
 
 // Verbindung zur Datenbank herstellen
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// Verbindung prüfen
-if ($conn->connect_error) {
- die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+function get_connection() {
+ $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+ if ($conn->connect_error) {
+  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
+ }
+ return $conn;
 }
