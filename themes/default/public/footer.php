0081613ec2b7799337156b03f02de484cc5e4fe0
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-03 20:00:24 +0100

    Update footer.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- themes/default/public/footer.php
+++ themes/default/public/footer.php
@@ -1,11 +1,5 @@
 <?php
-/* MiniGreX 1.0 /themes/default/public/footer.php */
-// load init.php
-require_once "init.php";
-// get database connection
-$conn = get_connection();
-// Load sitetitel and Meta-informationen
-$site_info = get_site_info($conn); 
+
 // create footer
 $html = "<footer class="pt-5 my-5 text-muted border-top">";
 $html = " Created with MiniGreX &copy; 2023";
@@ -17,5 +11,4 @@ $html .= "</body>
 </html>";
 // HTML-Output ausgeben
 print($html);
-// Verbindung zur Datenbank schließen
-$conn->close();
+
