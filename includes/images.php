b5138fc772d0a40df3ff12e3d569f4d2bc7bcf69
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-03 18:00:34 +0100

    Update images.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/images.php
+++ includes/images.php
@@ -1,2 +1,13 @@
 
 <?php // optimize images
+
+/*
+To do
+
+- Auto image ratio
+-Auto image responsive
+- auto image attrebute
+- image optimize - config for admin
+- function see images in small gallery (always 25 with infos and azy load in backend too
+
+*/
