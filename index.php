16a911c6acbdb5cd2a80d3811712a77f987a374c
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-08 22:00:15 +0100

    Update index.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 // load init.php
 require_once "includes/init.php";
-
-// Verbindung zur Datenbank herstellen
-function get_connection() {
- $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
- if ($conn->connect_error) {
-  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
- }
- return $conn;
+// Set up PDO connection
+try {
+    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
+// Load template
+require_once "/themes/theme_loader.php"; // general core functions
