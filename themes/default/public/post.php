1b9aeba401ac2b5c4ad922291dc93475654c36cc
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-08 21:44:22 +0100

    Update post.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- themes/default/public/post.php
+++ themes/default/public/post.php
@@ -6,16 +6,12 @@
  *
  * @package    default
  */
-include 'header.php';
-include 'menu.php';
 $html = "<main>;"
 $html = "<h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>"; 
 $html = "to do, get content";
 $html = "</main>";
 // HTML-Output 
 print($html);
-include 'footer.php';
-
 
 
 
