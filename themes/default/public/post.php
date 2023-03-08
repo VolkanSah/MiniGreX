ae01c06c43397828bf013148d419988616f50e29
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-08 21:32:20 +0100

    Update post.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- themes/default/public/post.php
+++ themes/default/public/post.php
@@ -1,2 +1,21 @@
 <?php
-/* MiniGreX 1.0 /themes/default/public/post.php */
+include 'header.php';
+include 'menu.php';
+$html = "<main>;
+$html = "<h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>"; 
+
+$html = "to do, get content";
+
+
+$html = "</main>";
+// HTML-Output 
+print($html);
+include 'footer.php';
+
+
+
+
+
+
+include 'sidebar.php';
+include 'footer.php';
