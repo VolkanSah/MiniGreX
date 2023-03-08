abd79222316365cf4bf1a899056931fe009fcbf6
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-08 21:43:57 +0100

    Update site.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- themes/default/public/site.php
+++ themes/default/public/site.php
@@ -6,12 +6,9 @@
  *
  * @package    default
  */
-include 'header.php';
-include 'menu.php';
 $html = "<main>;"
 $html = "<h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>"; 
 $html = "to do, get content";
 $html = "</main>";
 // HTML-Output 
 print($html);
-include 'footer.php';
