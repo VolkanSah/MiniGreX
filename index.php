0f186b58b193a33b96dd81990e2636e53233cc5c
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-15 14:15:08 +0100

    Update index.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 // load init.php
 require_once "includes/security.php";
-require_once "includes/upload.php";
-require_once "includes/upload.php";
+require_once "plugins/plugin_loader.php";
 require_once "themes/theme_loader.php";
+require_once "includes/upload.php";
 
