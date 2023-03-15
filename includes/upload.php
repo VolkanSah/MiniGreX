e7142dbdfc008526a867f76f384022b519a8abe7
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-15 14:03:16 +0100

    Update upload.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/upload.php
+++ includes/upload.php
@@ -1,74 +1,78 @@
-
-<!-- form tu use for upload .php
-<form action="upload.php" method="POST" enctype="multipart/form-data">
-    <input type="file" name="image" accept="image/*" />
-    <button type="submit">Upload</button>
-</form>
--->
-
 <?php
-// load init.php
-require_once "init.php";
+/*
+In this usage of the upload function, the upload_type is passed as a POST variable and then used to set the upload path and determine the allowed file types. 
+The files are then saved in the appropriate folder and saved in the database
+*/
 
-// Get reference to uploaded image
-$image_file = $_FILES["image"];
-// Get reference to uploaded video
-$video_file = $_FILES["video"];
+// include "init.php"
+require_once("init.php");
 
-// Exit if no file uploaded
-if (!isset($image_file)) {
-    die('No file uploaded.');
-}
-// Exit if no file uploaded
-if (!isset($video_file)) {
-    die('No file uploaded.');
-}
+// upload folder
+$upload_dir = "uploads/";
 
-// Exit if image file is zero bytes
-if (filesize($image_file["tmp_name"]) <= 0) {
-    die('Uploaded file has no contents.');
-}
-// Exit if image file is zero bytes
-if (filesize($video_file["tmp_name"]) <= 0) {
-    die('Uploaded file has no contents.');
+// check file extension
+function get_file_extension($file_name) {
+    return strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 }
 
-// Exit if is not a valid image file
-$image_type = exif_imagetype($image_file["tmp_name"]);
-if (!$image_type) {
-    die('Uploaded file is not an image.');
-}
-// Exit if is not a valid image file
-$video_type = exif_videotype($video_file["tmp_name"]);
-if (!$image_type) {
-    die('Uploaded file is not an video.');
+// generate secure filename
+function generate_safe_filename($file_name) {
+    $file_name = preg_replace("/[^A-Za-z0-9 \.\-_]/", '', $file_name);
+    $file_name = strtolower(str_replace(' ', '-', $file_name));
+    $file_name = uniqid() . '_' . $file_name;
+    return $file_name;
 }
 
-// Get file extension based on file type, to prepend a dot we pass true as the second parameter
-$image_extension = image_type_to_extension($image_type, true);
-$video_extension = video_type_to_extension($video_type, true);
+// audit user
+if (!isset($_SESSION['user_id'])) {
+    die("Unauthorized");
+}
 
-// Create a unique name
-$image_name = bin2hex(random_bytes(16)) . $image_extension;
-$video_name = bin2hex(random_bytes(16)) . $video_extension;
+// check if is 
+if (!isset($_FILES['file']) || empty($_FILES['file']['name'])) {
+    die("No file uploaded");
+}
 
+// Überprüfen, ob die hochgeladene Datei auf dem Server verarbeitet werden kann
+if (!is_uploaded_file($_FILES['file']['tmp_name'])) {
+    die("File could not be processed");
+}
 
-// Move the temp image file to the images directory
-move_uploaded_file(
-    // Temp image location
-    $image_file["tmp_name"],
+// Überprüfen, ob die Dateiendung erlaubt ist
+$extension = get_file_extension($_FILES['file']['name']);
 
-    // New image location
-    __DIR__ . "./uploads/image/" . $image_name
-);
-// Move the temp video file to the images directory
-move_uploaded_file(
-    // Temp image location
-    $video_file["tmp_name"],
+// Je nach Typ des Uploads den Ordnerpfad setzen
+if ($_POST['upload_type'] == 'image') {
+    $upload_path = $upload_dir . 'image/';
+    $allowed_types = array('jpg', 'jpeg', 'png', 'gif');
+} elseif ($_POST['upload_type'] == 'video') {
+    $upload_path = $upload_dir . 'video/';
+    $allowed_types = array('mp4', 'avi', 'mov');
+} else {
+    die("Invalid upload type");
+}
 
-    // New image location
-    __DIR__ . "./uploads/video/" . $image_name
-);
+if (!in_array($extension, $allowed_types)) {
+    die("Invalid file type");
+}
 
-// Redirect back to the page where the form was submitted from
-header('Location: ' . $_SERVER['HTTP_REFERER']);
+// Generieren eines sicheren Dateinamens
+$file_name = generate_safe_filename($_FILES['file']['name']);
+
+// Dateipfad für den Upload
+$file_path = $upload_path . $file_name;
+
+// Verschieben der Datei in das Upload-Verzeichnis
+if (!move_uploaded_file($_FILES['file']['tmp_name'], $file_path)) {
+    die("File could not be uploaded");
+}
+
+// Speichern des Uploads in der Datenbank
+$stmt = $pdo->prepare("INSERT INTO uploads (file_name, file_path, user_id, upload_type) VALUES (:file_name, :file_path, :user_id, :upload_type)");
+$stmt->bindParam(':file_name', $file_name, PDO::PARAM_STR);
+$stmt->bindParam(':file_path', $file_path, PDO::PARAM_STR);
+$stmt->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
+$stmt->bindParam(':upload_type', $_POST['upload_type'], PDO::PARAM_STR);
+$stmt->execute();
+
+echo "File uploaded successfully";
