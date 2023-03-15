5bf6a85c1eb5a6bcb80cbf53fdda0e9d57c38bd7
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-15 14:13:44 +0100

    Update index.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
 // load init.php
-require_once "includes/init.php";
-// Set up PDO connection
-try {
-    $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-// Load template
-require_once "/themes/theme_loader.php"; // general core functions
-// Verbindung zur Datenbank schließen
-$conn->close();
+require_once "includes/security.php";
+require_once "includes/upload.php";
+require_once "includes/upload.php";
+require_once "themes/theme_loader.php";
+
