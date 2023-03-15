ce6ade0b4c235e5a99d3ef799f214a523ed139ef
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-15 14:08:10 +0100

    Update upload.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/upload.php
+++ includes/upload.php
@@ -11,7 +11,6 @@ $upload_dir = "uploads/";
 function get_file_extension($file_name) {
     return strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 }
-
 // Function to generate a secure filename
 function generate_safe_filename($file_name) {
     $file_name = preg_replace("/[^A-Za-z0-9 \.\-_]/", '', $file_name);
@@ -19,25 +18,20 @@ function generate_safe_filename($file_name) {
     $file_name = uniqid() . '_' . $file_name;
     return $file_name;
 }
-
 // Check if a user is authenticated
 if (!isset($_SESSION['user_id'])) {
     die("Unauthorized");
 }
-
 // Check if a file has been uploaded
 if (!isset($_FILES['file']) || empty($_FILES['file']['name'])) {
     die("No file uploaded");
 }
-
 // Check if the uploaded file can be processed on the server
 if (!is_uploaded_file($_FILES['file']['tmp_name'])) {
     die("File could not be processed");
 }
-
 // Check if the file extension is allowed
 $extension = get_file_extension($_FILES['file']['name']);
-
 // Set the folder path depending on the type of upload
 if ($_POST['upload_type'] == 'image') {
     $upload_path = $upload_dir . 'image/';
@@ -52,18 +46,14 @@ if ($_POST['upload_type'] == 'image') {
 if (!in_array($extension, $allowed_types)) {
     die("Invalid file type");
 }
-
 // Generate a secure filename
 $file_name = generate_safe_filename($_FILES['file']['name']);
-
 // File path for upload
 $file_path = $upload_path . $file_name;
-
 // move the file to the uploads directory
 if (!move_uploaded_file($_FILES['file']['tmp_name'], $file_path)) {
     die("File could not be uploaded");
 }
-
 // save the upload to the database
 $stmt = $pdo->prepare("INSERT INTO uploads (file_name, file_path, user_id, upload_type) VALUES (:file_name, :file_path, :user_id, :upload_type)");
 $stmt->bindParam(':file_name', $file_name, PDO::PARAM_STR);
@@ -71,5 +61,5 @@ $stmt->bindParam(':file_path', $file_path, PDO::PARAM_STR);
 $stmt->bindParam(':user_id', $_SESSION['user_id'], PDO::PARAM_INT);
 $stmt->bindParam(':upload_type', $_POST['upload_type'], PDO::PARAM_STR);
 $stmt->execute();
-
 echo "File uploaded successfully";
+// end security.php
