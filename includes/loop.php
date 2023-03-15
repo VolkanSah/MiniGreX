f89946aa5d3534d6ad8ddea761a20a3802e813d5
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-15 14:20:51 +0100

    Update loop.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/loop.php
+++ includes/loop.php
@@ -1,41 +1,79 @@
-<?php /**
- * This is the loop file, so it is more simple to edit it or call 
- * This file is used to markup the loop aspects of the MiniGreX.
- *
- * @link       includes/loop.php
- * @since      1.0.0
- *
- * @package    core
- */
-// load init.php
-require_once "init.php";
-// Load sitetitel and Meta-informationen
-$site_info = get_site_info($conn);
-// Start Loop
-// load all pots from database
-$posts = get_all_posts($conn);
-// show all posts
-foreach ($posts as $post) {
-  $comments = get_comments_for_post($conn, $post['id']);
-  $username = get_username($conn, $post['benutzer_id']);
-  $html .= "<h2>" . htmlspecialchars($post['link'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</h2>";
-  foreach ($comments as $comment) {
-    $username = get_username($conn, $comment['benutzer_id']);
-    $html .= "<p>" . htmlspecialchars($comment['text'], ENT_QUOTES) . " von " . htmlspecialchars($username, ENT_QUOTES) . "</p>";
-  }
-  if (is_logged_in()) {
-    $html .= "<form method='post' action='add_comment.php'>
-      <input type='hidden' name='post_id' value='" . htmlspecialchars($post['id'], ENT_QUOTES) . "'>
-      <label for='text'>Comment:</label>
-      <input type='text' id='text' name='text'>
-      <br>
-      <input type='submit' value='Submit'>
-    </form>";
-  }
+<?php
+
+// include init.php
+require_once("init.php");
+
+// Definieren der Variablen
+$category = isset($_GET['category']) ? $_GET['category'] : 'all';
+$status = 'published';
+$limit = 10;
+$page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
+$offset = ($page - 1) * $limit;
+
+// Ausführen der Datenbankabfrage mit Prepared Statements
+$stmt = $pdo->prepare("SELECT * FROM articles WHERE category = :category AND status = :status ORDER BY date DESC LIMIT :offset, :limit");
+$stmt->bindParam(':category', $category, PDO::PARAM_STR);
+$stmt->bindParam(':status', $status, PDO::PARAM_STR);
+$stmt->bindParam(':offset', $offset, PDO::PARAM_INT);
+$stmt->bindParam(':limit', $limit, PDO::PARAM_INT);
+$stmt->execute();
+
+// Speichern der Abfrageergebnisse in einer Variable
+$articles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Ausgabe der Artikel in HTML-Formatierung
+foreach ($articles as $article) {
+    $article_title = htmlspecialchars($article['title']);
+    $article_content = htmlspecialchars($article['content']);
+    $article_date = date('Y-m-d', strtotime($article['date']));
+
+    $html = <<<HTML
+        <div class="article">
+            <h2>$article_title</h2>
+            <p>$article_content</p>
+            <span class="date">$article_date</span>
+        </div>
+    HTML;
+
+    echo $html;
 }
 
-// to doo loop  pagination comments and posts, set minmaximum for both not finished
-// end loop
+// Erstellen der Pagination-Links
+$count_stmt = $pdo->prepare("SELECT COUNT(*) FROM articles WHERE category = :category AND status = :status");
+$count_stmt->bindParam(':category', $category, PDO::PARAM_STR);
+$count_stmt->bindParam(':status', $status, PDO::PARAM_STR);
+$count_stmt->execute();
+$total_articles = $count_stmt->fetchColumn();
 
-// HTML-Output
-print($html);
+$total_pages = ceil($total_articles / $limit);
+$prev_page = ($page > 1) ? ($page - 1) : false;
+$next_page = ($page < $total_pages) ? ($page + 1) : false;
+
+$html = <<<HTML
+    <div class="pagination">
+HTML;
+
+if ($prev_page !== false) {
+    $html .= <<<HTML
+        <a href="?category=$category&page=$prev_page">&laquo; Previous</a>
+    HTML;
+}
+
+for ($i = 1; $i <= $total_pages; $i++) {
+    $class = ($i == $page) ? 'active' : '';
+    $html .= <<<HTML
+        <a href="?category=$category&page=$i" class="$class">$i</a>
+    HTML;
+}
+
+if ($next_page !== false) {
+    $html .= <<<HTML
+        <a href="?category=$category&page=$next_page">Next &raquo;</a>
+    HTML;
+}
+
+$html .= <<<HTML
+    </div>
+HTML;
+
+echo $html;
