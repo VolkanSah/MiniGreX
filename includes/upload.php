d398cf39bd6f13678cccc6ee027658de7cc3760f
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-03-15 14:07:21 +0100

    Update upload.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/upload.php
+++ includes/upload.php
@@ -3,19 +3,16 @@
 In this usage of the upload function, the upload_type is passed as a POST variable and then used to set the upload path and determine the allowed file types. 
 The files are then saved in the appropriate folder and saved in the database
 */
-
 // include "init.php"
 require_once("init.php");
-
-// upload folder
+// Directory for uploads
 $upload_dir = "uploads/";
-
-// check file extension
+// Funktion zum Überprüfen der Dateiendung
 function get_file_extension($file_name) {
     return strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
 }
 
-// generate secure filename
+// Function to generate a secure filename
 function generate_safe_filename($file_name) {
     $file_name = preg_replace("/[^A-Za-z0-9 \.\-_]/", '', $file_name);
     $file_name = strtolower(str_replace(' ', '-', $file_name));
@@ -23,25 +20,25 @@ function generate_safe_filename($file_name) {
     return $file_name;
 }
 
-// audit user
+// Check if a user is authenticated
 if (!isset($_SESSION['user_id'])) {
     die("Unauthorized");
 }
 
-// check if is 
+// Check if a file has been uploaded
 if (!isset($_FILES['file']) || empty($_FILES['file']['name'])) {
     die("No file uploaded");
 }
 
-// Überprüfen, ob die hochgeladene Datei auf dem Server verarbeitet werden kann
+// Check if the uploaded file can be processed on the server
 if (!is_uploaded_file($_FILES['file']['tmp_name'])) {
     die("File could not be processed");
 }
 
-// Überprüfen, ob die Dateiendung erlaubt ist
+// Check if the file extension is allowed
 $extension = get_file_extension($_FILES['file']['name']);
 
-// Je nach Typ des Uploads den Ordnerpfad setzen
+// Set the folder path depending on the type of upload
 if ($_POST['upload_type'] == 'image') {
     $upload_path = $upload_dir . 'image/';
     $allowed_types = array('jpg', 'jpeg', 'png', 'gif');
@@ -56,18 +53,18 @@ if (!in_array($extension, $allowed_types)) {
     die("Invalid file type");
 }
 
-// Generieren eines sicheren Dateinamens
+// Generate a secure filename
 $file_name = generate_safe_filename($_FILES['file']['name']);
 
-// Dateipfad für den Upload
+// File path for upload
 $file_path = $upload_path . $file_name;
 
-// Verschieben der Datei in das Upload-Verzeichnis
+// move the file to the uploads directory
 if (!move_uploaded_file($_FILES['file']['tmp_name'], $file_path)) {
     die("File could not be uploaded");
 }
 
-// Speichern des Uploads in der Datenbank
+// save the upload to the database
 $stmt = $pdo->prepare("INSERT INTO uploads (file_name, file_path, user_id, upload_type) VALUES (:file_name, :file_path, :user_id, :upload_type)");
 $stmt->bindParam(':file_name', $file_name, PDO::PARAM_STR);
 $stmt->bindParam(':file_path', $file_path, PDO::PARAM_STR);
