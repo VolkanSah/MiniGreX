574930f3ccd09ce8fe06a54212846c2c05d57163
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-04-01 11:20:20 +0200

    Update functions.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- includes/functions.php
+++ includes/functions.php
@@ -2,15 +2,6 @@
 // load init.php
 require_once "init.php";
 
-// Verbindung zur Datenbank herstellen
-function get_connection() {
- $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
- if ($conn->connect_error) {
-  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
- }
- return $conn;
-}
-
 // Ad user
 function add_user($username, $password) {
   $hash = password_hash($password, PASSWORD_DEFAULT);
