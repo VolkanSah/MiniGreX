10ef4b15695069788938be95b9d15f882a5ffb0a
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-04-01 12:57:50 +0200

    Update cache_db.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- cache/cache_db.php
+++ cache/cache_db.php
@@ -1,44 +1,97 @@
 <?PHP
 
-// location and prefix for cache files
-define('CACHE_PATH_HTML', "/html");
-define('CACHE_PATH_CSS', "/css");
-define('CACHE_PATH_JS', "/js");
-// how long to keep the cache files (hours)
+// Location and prefix for cache files
+define('CACHE_PATH', "cache/");
+define('CACHE_PATH_POST', CACHE_PATH . "posts");
+define('CACHE_PATH_PAGE', CACHE_PATH . "pages");
+define('CACHE_PATH_UPLOAD', CACHE_PATH . "uploads");
+
+// How long to keep the cache files (hours)
 define('CACHE_TIME', 24);
-// return location and name for cached files
-  function cache_file()
-  {
-    return CACHE_PATH . md5($_SERVER['REQUEST_URI']);
-  }
 
-  //  if present and not expired show cached files
-  function cache_display()
-  {
-    $file = cache_file();
+// Return location and name for cached post
+function cache_file_post($id)
+{
+    return CACHE_PATH_POST . "/" . $id . ".html";
+}
 
-    // check if file exists & timestamp
-    if(!file_exists($file)) return;
-    if(filemtime($file) < time() - CACHE_TIME * 3600) return;
+// Return location and name for cached page
+function cache_file_page($slug)
+{
+    return CACHE_PATH_PAGE . "/" . $slug . ".html";
+}
 
-    // if display cache file and stop processing
-    echo gzuncompress(file_get_contents($file));
+// Return location and name for cached upload
+function cache_file_upload($file_path)
+{
+    return CACHE_PATH_UPLOAD . "/" . md5($file_path) . ".html";
+}
+
+// If present and not expired show cached files
+function cache_display()
+{
+    $uri = $_SERVER['REQUEST_URI'];
+    $file = '';
+
+    // Check if URI is for a post or page
+    if (strpos($uri, '/post/') !== false) {
+        // URI is for a post
+        $post_id = explode('/', $uri)[2];
+        $file = cache_file_post($post_id);
+    } elseif (strpos($uri, '/page/') !== false) {
+        // URI is for a page
+        $slug = explode('/', $uri)[2];
+        $file = cache_file_page($slug);
+    } elseif (strpos($uri, '/uploads/') !== false) {
+        // URI is for an upload
+        $file_path = explode('/', $uri)[2];
+        $file = cache_file_upload($file_path);
+    }
+
+    // Check if file exists and timestamp
+    if (!file_exists($file)) {
+        return;
+    }
+    if (filemtime($file) < time() - CACHE_TIME * 3600) {
+        return;
+    }
+
+    // Display cache file and stop processing
+    echo file_get_contents($file);
     exit;
-  }
+}
 
-  // write cache files
-  function cache_page($content)
-  {
-    if(false !== ($f = @fopen(cache_file(), 'w'))) {
-      fwrite($f, gzcompress($content));	
-      fclose($f);
+// Write cache files
+function cache_page($content)
+{
+    $uri = $_SERVER['REQUEST_URI'];
+    $file = '';
+
+    // Check if URI is for a post or page
+    if (strpos($uri, '/post/') !== false) {
+        // URI is for a post
+        $post_id = explode('/', $uri)[2];
+        $file = cache_file_post($post_id);
+    } elseif (strpos($uri, '/page/') !== false) {
+        // URI is for a page
+        $slug = explode('/', $uri)[2];
+        $file = cache_file_page($slug);
+    } elseif (strpos($uri, '/uploads/') !== false) {
+        // URI is for an upload
+        $file_path = explode('/', $uri)[2];
+        $file = cache_file_upload($file_path);
+    }
+
+    // Write content to cache file
+    if (false !== ($f = @fopen($file, 'w'))) {
+        fwrite($f, $content);
+        fclose($f);
     }
     return $content;
-  }
+}
 
-  // execution stops here if valid cache file found
-  cache_display();
+// Execution stops here if valid cache file found
+cache_display();
 
-  // enable output buffering and create cache file
-  ob_start('cache_page');
-
+// Enable output buffering and create cache file
+ob_start('cache_page');
