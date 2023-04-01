0e9f616db32b3519e43652a020284ef41dcefef4
Volkan Sah <VolkanSah@users.noreply.github.com>
2023-04-01 12:39:08 +0200

    Update register.php
    Signed-off-by: Volkan Sah <VolkanSah@users.noreply.github.com>

--- register.php
+++ register.php
@@ -1,16 +1,6 @@
 <?php
 // load init.php
 require_once "includes/init.php";
-
-// Verbindung zur Datenbank herstellen
-function get_connection() {
- $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
- if ($conn->connect_error) {
-  die("Verbindung zur Datenbank fehlgeschlagen: " . $conn->connect_error);
- }
- return $conn;
-}
-
 // Formularabschicken überprüfen
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = $_POST['username'];
