04502977e90f482dcd4dafd30af9b8d5de861089
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-13 16:47:06 +0200

    Update theme_loader.php

--- themes/theme_loader.php
+++ themes/theme_loader.php
@@ -23,7 +23,28 @@ require_once "default/public/sidebar_site.php"; // load menu.php
 require_once "default/public/site.php"; // load menu.php
 require_once "default/public/footer.php"; // load footer.php
 }
-
+   
+// load css
+   
+if( is_default_css() {
+require_once "default/public/css/default.css"; // load css
+  
+  // dosomework with it ! soon
+  
+  // print!
+}
+   
+   // load css
+   
+if( is_default_js() {
+require_once "default/public/js/default.js"; // load css
+  
+  // do somework with it ! soon
+  
+  // print!
+}
+   
+   
 
 
 
