727b7043c037a8f92178dae42a1634855cc259b3
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-13 16:58:40 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php //not dev! wait i think!
-// load init.php
-require_once "includes/security.php";
-require_once "plugins/plugin_loader.php";
-require_once "themes/theme_loader.php";
+// load init.php no!all in init.php
+//require_once "includes/security.php";
+//require_once "plugins/plugin_loader.php";
+//require_once "themes/theme_loader.php";
 //require_once "includes/upload.php";
 
+
+// thinking: request>ident>parse>recheck>get_request>fallback>log>testcon>test_Security>blacklist>whitelist>deepmode?>2ban_or_acc_work>fin
