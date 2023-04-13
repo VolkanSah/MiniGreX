7e20b669f77150b323c8a4bdfa81d3bb4ea23728
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-13 16:41:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<?php
+<?php //not dev! wait i think!
 // load init.php
 require_once "includes/security.php";
 require_once "plugins/plugin_loader.php";
 require_once "themes/theme_loader.php";
-require_once "includes/upload.php";
+//require_once "includes/upload.php";
 
