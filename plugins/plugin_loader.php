648267cf22a4534f036992529fcce03d8c5963ee
Volkan Küçükbudak <VolkanSah@users.noreply.github.com>
2023-04-13 17:06:09 +0200

    Update plugin_loader.php

--- plugins/plugin_loader.php
+++ plugins/plugin_loader.php
@@ -1,7 +1,8 @@
 <?php
-/*  MiniGreX 1.0 Plugin Loader with stmt & pod not finished !*/
+/*  MiniGreX 1.0 Plugin Loader for stmt & pod not finished !*/
 
-
+// needed! DOM_load wait
+// needed! check_errors_ >if>else>stop!
 // load your plugin_core here for init
 require_once "role_manager/core.php";
 require_once "gateway_manager/core.php";
@@ -12,7 +13,7 @@ require_once "video_db/core.php";
 require_once "role_manager/core.php";
 
 
-// Load plugins 
+// Load plugins ??
 $seo_manager = get_site_seo_plugin($conn);
 $image_db = get_site_image_plugin($conn);
 $video_db = get_site_video_plugin($conn);
