6342295c20a3aa59e53e8c2577ddb2c303908099
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-05-02 17:41:20 +0200

    Finished theme_loader.php

--- themes/theme_loader.php
+++ themes/theme_loader.php
@@ -1,52 +1,21 @@
 <?php
-// Load template if its home
-// careful! not finished... only for remember!
-if( is_front_page() {
-require_once "default/public/header.php"; // load header.php
-require_once "default/public/menu.php"; // load menu.php
-require_once "default/public/home.php"; // load menu.php
-require_once "default/public/footer.php"; // load footer.php
-}
-   // else > error!
-// Load template if its post
-if( is_post_page() {
-require_once "default/public/header.php"; // load header.php
-require_once "default/public/menu.php"; // load menu.php
-require_once "default/public/sidebar_post.php"; // load menu.php
-require_once "default/public/post.php"; // load menu.php
-require_once "default/public/footer.php"; // load footer.php
-}
-// Load template if its site
-if( is_site_page() {
-require_once "default/public/header.php"; // load header.php
-require_once "default/public/menu.php"; // load menu.php
-require_once "default/public/sidebar_site.php"; // load menu.php
-require_once "default/public/site.php"; // load menu.php
-require_once "default/public/footer.php"; // load footer.php
-}
-   
-// load css
-     // else > error!
-if( is_default_css() {
-require_once "default/css/default.css"; // load css
-  
-  // do somework with it ! soon
-  // fontO /bootstrap />load local >check
-  // print!
-}
-   
-   // load css
-   
-if( is_default_js() {
-require_once "default/js/default.js"; // load js
-  
-  // do somework with it ! soon
-     // fontO /bootstrap />load local >check
+// NOW FINISHED! So we can call what we need where we need
+// Location and prefix for Theme-files
+define('TEMPLATE_PATH', "default");
+define('TEMPLATE_CSS_PATH', TEMPLATE_PATH . "css");
+define('TEMPLATE_JS_PATH', TEMPLATE_PATH . "js");
+define('TEMPLATE_HEADER', TEMPLATE_PATH . "header.php");
+define('TEMPLATE_MENU', TEMPLATE_PATH . "menu.php");
+define('TEMPLATE_HOME', TEMPLATE_PATH . "home.php");
+define('TEMPLATE_FOOTER', TEMPLATE_PATH . "footer.php");
+define('TEMPLATE_POST', TEMPLATE_PATH . "post.php");
+define('TEMPLATE_POST_SIDEBAR', TEMPLATE_PATH . "sidebar_post.php");
+define('TEMPLATE_SITE', TEMPLATE_PATH . "site.php");
+define('TEMPLATE_SITE_SIDEBAR', TEMPLATE_PATH . "sidebar_site.php");
+define('TEMPLATE_CSS', TEMPLATE_CSS_PATH . "default.css");
+define('TEMPLATE_JS', TEMPLATE_JS_PATH . "default.js");
+// load your own stufffor themes
 
-  
-  // print!
-}
-   
    
 
 
