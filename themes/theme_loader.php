e47474e9d9c7ae9627bec99c714c712d6f808acd
Volkan Kücükbudak <VolkanSah@users.noreply.github.com>
2023-05-25 20:02:48 +0200

    Update theme_loader.php

--- themes/theme_loader.php
+++ themes/theme_loader.php
@@ -4,14 +4,14 @@
 define('TEMPLATE_PATH', "default");
 define('TEMPLATE_CSS_PATH', TEMPLATE_PATH . "css");
 define('TEMPLATE_JS_PATH', TEMPLATE_PATH . "js");
-define('TEMPLATE_HEADER', TEMPLATE_PATH . "header.php");
-define('TEMPLATE_MENU', TEMPLATE_PATH . "menu.php");
-define('TEMPLATE_HOME', TEMPLATE_PATH . "home.php");
-define('TEMPLATE_FOOTER', TEMPLATE_PATH . "footer.php");
-define('TEMPLATE_POST', TEMPLATE_PATH . "post.php");
-define('TEMPLATE_POST_SIDEBAR', TEMPLATE_PATH . "sidebar_post.php");
-define('TEMPLATE_SITE', TEMPLATE_PATH . "site.php");
-define('TEMPLATE_SITE_SIDEBAR', TEMPLATE_PATH . "sidebar_site.php");
+define('TEMPLATE_HEADER', TEMPLATE_PATH . "public/header.php");
+define('TEMPLATE_MENU', TEMPLATE_PATH . "public/menu.php");
+define('TEMPLATE_HOME', TEMPLATE_PATH . "public/home.php");
+define('TEMPLATE_FOOTER', TEMPLATE_PATH . "public/footer.php");
+define('TEMPLATE_POST', TEMPLATE_PATH . "public/post.php");
+define('TEMPLATE_POST_SIDEBAR', TEMPLATE_PATH . "publi/sidebar_post.php");
+define('TEMPLATE_SITE', TEMPLATE_PATH . "public/site.php");
+define('TEMPLATE_SITE_SIDEBAR', TEMPLATE_PATH . "public/sidebar_site.php");
 define('TEMPLATE_CSS', TEMPLATE_CSS_PATH . "default.css");
 define('TEMPLATE_JS', TEMPLATE_JS_PATH . "default.js");
 // load your own stufffor themes
