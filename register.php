b0a59d2ff704d4406e728e51883b6742dbba2053
Volkan Şah //NCF <VolkanSah@users.noreply.github.com>
2023-07-13 01:49:04 +0200

    Update register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 // load init.php
-require_once "includes/init.php";
+require_once INIT_MGREX;
 // Formularabschicken überprüfen
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $username = $_POST['username'];
