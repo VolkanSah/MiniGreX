808afaef80efed1bc8b52734a6fcdf16a08f0e04
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 01:13:01 +0200

    Update footer.php

--- panel/footer.php
+++ panel/footer.php
@@ -1,3 +1,19 @@
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    panel/footer.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ *  Admin : Footer
+ * 
+ * 
+ */
 <footer>
     <div class="container">
         <p>&copy; <?php echo date('Y'); ?> MiniGreX CMS. All rights reserved.</p>
