f7832f30cb981cd7925a0d93157c036a3bc2f3e8
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 01:13:43 +0200

    Update header.php

--- panel/header.php
+++ panel/header.php
@@ -1,4 +1,19 @@
-<header>
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    panel/header
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ *  Admin : Header
+ * 
+ * 
+ */<header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="dashboard.php">MiniGreX CMS</a>
