0b009c950f6039dee46e4baf914bed49d0e4e53a
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-07 22:36:19 +0200

    Update header.php

--- themes/default/public/header.php
+++ themes/default/public/header.php
@@ -5,6 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Blog</title>
     <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/default.css">
 </head>
 <body>
-
