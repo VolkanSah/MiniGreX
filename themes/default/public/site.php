1034b2ea92cc95b8012e7614684ba84f8da88855
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 00:53:06 +0200

    Update site.php

--- themes/default/public/site.php
+++ themes/default/public/site.php
@@ -1,14 +1,16 @@
-<?php /**
- * This is the page template file, so it is more simple to edit it or call this file is used to markup the loop aspects of the MiniGreX.
- * not finished!to do- get content & styles
- * @link       themes/default/public/post.php
- * @since      1.0.0
+<?php
+/**
  *
- * @package    default
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    themes/default/public/menu.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ * This is the public facing site
  */
-$html = "<main>;"
-$html = "<h1>" . htmlspecialchars($site_info['title'], ENT_QUOTES) . "</h1>"; 
-$html = "to do, get content";
-$html = "</main>";
-// HTML-Output 
-print($html);
+?>
+
