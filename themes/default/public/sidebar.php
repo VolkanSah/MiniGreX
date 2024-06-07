e0e51f2896920aa76ed66003bb8e934f2833d9cf
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 00:52:42 +0200

    Update sidebar.php

--- themes/default/public/sidebar.php
+++ themes/default/public/sidebar.php
@@ -1,3 +1,18 @@
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    themes/default/public/menu.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ * This is the public facing sidebar
+ */
+?>
 <div class="col-md-4">
     <h2>Sidebar</h2>
     <p>Additional content here.</p>
