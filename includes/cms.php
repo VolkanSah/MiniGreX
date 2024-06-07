a351ca9ced2e5f4e5385f0405c37a046f75f1d02
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 01:05:14 +0200

    Update cms.php

--- includes/cms.php
+++ includes/cms.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    includes/cms.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ *  Class CMS
+ * 
+ */
 
 class CMS {
     private $pdo;
