928694b762e01cfb585f76167557cd2740d002c7
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 00:46:39 +0200

    Update footer.php

--- themes/default/public/footer.php
+++ themes/default/public/footer.php
@@ -1,3 +1,17 @@
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    themes/default/public/footer.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ * This is the public facing footer
+ */
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
         <span class="text-muted">&copy; <?php echo date('Y'); ?> My Blog. All rights reserved.</span>
