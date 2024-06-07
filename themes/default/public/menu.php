19c4b9f3c003d4b5bc4470402f2cfd97401c9fb1
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 00:51:57 +0200

    Update menu.php

--- themes/default/public/menu.php
+++ themes/default/public/menu.php
@@ -3,14 +3,14 @@
  *
  * MiniGreX - designed with security in mind!
  *
- * @file    themes/default/public/index.php
+ * @file    themes/default/public/menu.php
  * @package MiniGreX
  * @copyright  Volkan Kücükbudak 
  * @version 0.9 Beta
  * @license MIT
  * @link    https://github.com/VolkanSah/MiniGreX
  *
- * silence is golden
+ * This is the public facing menu
  */
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
