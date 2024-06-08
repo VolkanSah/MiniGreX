870e6e08fc7522f44808232feade2758aa76cafd
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 19:01:56 +0200

    Create profile_manager.php

---
+++ plugins/profile_manager/profile_manager.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    plugins/profile_manager/profile_manager.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ * Profile Manger Facing file
+ */
