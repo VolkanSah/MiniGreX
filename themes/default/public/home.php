a619ca9e64d65d015248eac7f290a394571ff761
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 22:11:09 +0200

    Update home.php

--- themes/default/public/home.php
+++ themes/default/public/home.php
@@ -15,7 +15,7 @@
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
+        <div class="content col-md-8"> 
             <h1>Welcome to My Blog</h1>
             <p>This is the home page.</p>
         </div>
