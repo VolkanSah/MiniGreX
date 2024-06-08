6832ddcb756433df7b9e391d33829aebab8725e5
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 18:57:26 +0200

    Update footer.php

--- themes/default/public/footer.php
+++ themes/default/public/footer.php
@@ -15,7 +15,7 @@
 ?>
 <footer class="footer mt-auto py-3 bg-light">
     <div class="container">
-        <span class="text-muted">&copy; <?php echo date('Y'); ?> My Blog. All rights reserved.</span>
+        <span class="text-muted">&copy; <?php echo date('Y'); ?> <?php echo get('copyright'); ?> . All rights reserved.</span>
     </div>
 </footer>
 <script src="../js/jquery.min.js"></script>
