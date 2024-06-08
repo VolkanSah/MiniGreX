32f66c3676a6786a4976a02ebad16180d7fc4bb1
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 22:31:03 +0200

    Update header.php

--- panel/header.php
+++ panel/header.php
@@ -14,9 +14,5 @@
  * 
  * 
  */<header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="dashboard.php">MiniGreX CMS</a>
-        </div>
-    </nav>
+
 </header>
