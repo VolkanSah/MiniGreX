9826233587c6d103e5c61b7435d5992bb44c6b1b
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-06-08 15:17:07 +0200

    Update init.php

--- includes/init.php
+++ includes/init.php
@@ -15,7 +15,9 @@
  * Finally, it is checked whether the page is being accessed via HTTPS and, if necessary, a redirection to HTTPS is performed.
  * 
  */
+// #############################################
 // Note! set chmod to this file to 0444 or 0666
+// #############################################
 // Please setup your database informations
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'your_db_name');
@@ -54,51 +56,56 @@ try {
 // Define constants for DB tables
 define("USERS_TABLE", "users"); // developed !
 define("POSTS_TABLE", "posts"); // developed !
-define("SITES_TABLE", "sites");  // not developed !
+define("SITES_TABLE", "sites");  // not finished !
 define("COMMENTS_TABLE", "comments"); // developed !
 define("ROLES_TABLE", "roles"); // developed !
 define("USER_ROLES_TABLE", "user_roles"); // developed !
 define("PERMISSIONS_TABLE", "permissions"); // developed !
 
-
 // if exist define und uncoment
+
 //define("IMAGE_TABLE", "imagedbs"); // not developed !
 //define("VIDEO_TABLE", "videodbs"); // not developed !
-
 //define("SEO_TABLE", "seo_managers"); // not developed !
 //define("PROFILE_TABLE", "profile_managerr"); // not developed !
 //define("SECURITY_TABLE", "security_manager"); // not developed !
-//   Prepare statements for commonly used queries 
-// User prepared statements
+
+/*
+
+            Prepare statements for commonly used queries
+
+*/
+
+// User 
 $stmt_select_all_users = $pdo->prepare("SELECT * FROM " . USERS_TABLE);
 $stmt_select_user_by_id = $pdo->prepare("SELECT * FROM " . USERS_TABLE . " WHERE id = :id");
 $stmt_insert_user = $pdo->prepare("INSERT INTO " . USERS_TABLE . " (username, password) VALUES (:username, :password)");
 
-// Posts with category prepared statements
+// Posts with category 
 $stmt_select_all_posts = $pdo->prepare("SELECT * FROM " . POSTS_TABLE);
 $stmt_select_post_by_id = $pdo->prepare("SELECT * FROM " . POSTS_TABLE . " WHERE id = :id");
 $stmt_insert_post = $pdo->prepare("INSERT INTO " . POSTS_TABLE . " (user_id, link) VALUES (:user_id, :link)");
 
-// Comments prepared statements
+// Comments 
 $stmt_select_comments_by_post_id = $pdo->prepare("SELECT * FROM " . COMMENTS_TABLE . " WHERE post_id = :post_id");
 $stmt_insert_comment = $pdo->prepare("INSERT INTO " . COMMENTS_TABLE . " (post_id, text) VALUES (:post_id, :text)");
 
-// Sites with category prepared statements
+// Sites with category
 $stmt_select_all_sites = $pdo->prepare("SELECT * FROM " . SITES_TABLE);
 $stmt_select_sites_by_id = $pdo->prepare("SELECT * FROM " . SITES_TABLE . " WHERE id = :id");
 $stmt_insert_sites = $pdo->prepare("INSERT INTO " . SITES_TABLE . " (user_id, link) VALUES (:user_id, :link)");
 
-// image_db prepared statements
+// image_db 
 //$stmt_select_all_image_dbs = $pdo->prepare("SELECT * FROM " . IMAGE_TABLE);
 //$stmt_select_image_dbs_by_id = $pdo->prepare("SELECT * FROM " . IMAGE_TABLE . " WHERE id = :id");
 //$stmt_insert_image_dbs = $pdo->prepare("INSERT INTO " . IMAGE_TABLE . " (title, content, link, author_id, category) VALUES (:title, :content, :link, :author_id, :category)");
 
-// video_db prepared statements
+// video_db 
 //$stmt_select_all_video_dbs = $pdo->prepare("SELECT * FROM " . VIDEO_TABLE);
 //$stmt_select_video_dbs_by_id = $pdo->prepare("SELECT * FROM " . VIDEO_TABLE . " WHERE id = :id");
 //$stmt_insert_video_dbs = $pdo->prepare("INSERT INTO " . VIDEO_TABLE . " (title, content, link, author_id, category) VALUES (:title, :content, :link, :author_id, :category)");
 
-// Role Manager prepared statements
+// Role Manager 
 $stmt_select_all_roles = $pdo->prepare("SELECT * FROM " . ROLES_TABLE);
 $stmt_select_role_by_id = $pdo->prepare("SELECT * FROM " . ROLES_TABLE . " WHERE id = :id");
 $stmt_insert_role = $pdo->prepare("INSERT INTO " . ROLES_TABLE . " (name) VALUES (:name)");
@@ -111,4 +118,5 @@ $stmt_select_all_permissions = $pdo->prepare("SELECT * FROM " . PERMISSIONS_TABL
 $stmt_select_permission_by_role_id = $pdo->prepare("SELECT * FROM " . PERMISSIONS_TABLE . " WHERE role_id = :role_id");
 $stmt_insert_permission = $pdo->prepare("INSERT INTO " . PERMISSIONS_TABLE . " (role_id, permission) VALUES (:role_id, :permission)");
 
+// soon...
 ?>
