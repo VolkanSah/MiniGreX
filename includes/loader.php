425c909113549e809ab6e766b7f49664cbd7ae63
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-09-05 17:04:02 +0200

    Update loader.php

--- includes/loader.php
+++ includes/loader.php
@@ -1,27 +1,22 @@
 <?php
+// Dynamischer Pfad zum Root-Verzeichnis
+define('ROOT_PATH', __DIR__ . '/../');
 
-// Path definitions
-define('INIT_MGREX', __DIR__ . '/includes/init.php');
-define('SECURITY_MGREX', __DIR__ . '/includes/security.php');
-define('LOOP_MGREX', __DIR__ . '/includes/loop.php');
-define('FUNCTION_MGREX', __DIR__ . '/includes/functions.php');
-define('UPLOAD_MGREX', __DIR__ . '/includes/upload.php');
-define('IMAGES_MGREX', __DIR__ . '/includes/images.php');
-define('CMS_MGREX', __DIR__ . '/includes/cms.php');
-define('CACHE_MGREX', __DIR__ . '/includes/cache.php');
-define('PLUGIN_LOADER', __DIR__ . '/plugins/plugin_loader.php');
-define('THEME_LOADER', __DIR__ . '/themes/theme_loader.php');
+// Pfade zu den wichtigen Dateien
+define('INIT_MGREX', ROOT_PATH . 'includes/init.php');
+define('SECURITY_MGREX', ROOT_PATH . 'includes/security.php');
+define('LOOP_MGREX', ROOT_PATH . 'includes/loop.php');
+define('FUNCTION_MGREX', ROOT_PATH . 'includes/functions.php');
+define('UPLOAD_MGREX', ROOT_PATH . 'includes/upload.php');
+define('IMAGES_MGREX', ROOT_PATH . 'includes/images.php');
+define('CMS_MGREX', ROOT_PATH . 'includes/cms.php');
+define('CACHE_MGREX', ROOT_PATH . 'includes/cache.php');
+define('PLUGIN_LOADER', ROOT_PATH . 'plugins/plugin_loader.php');
+define('THEME_LOADER', ROOT_PATH . 'themes/theme_loader.php');
 
-// Include the init file
-require_once INIT_MGREX; // MiniGrex init
-require_once SECURITY_MGREX; // security init
-require_once LOOP_MGREX; // loop init
-require_once FUNCTION_MGREX; // functions init
-require_once UPLOAD_MGREX; // uplads init
-require_once IMAGES_MGREX; // images init
-require_once CMS_MGREX; // CMS init
-require_once CACHE_MGREX; // ccahe init
-require_once PLUGIN_LOADER; // plugin init (integration of plugins)
-require_once THEME_LOADER; // theme init (integration of plugins)
+// Lade die Initialisierungsdateien
+require_once INIT_MGREX;
+require_once SECURITY_MGREX;
+require_once PLUGIN_LOADER;
+require_once THEME_LOADER;
 ?>
-
