3c1395db9e960fb3fa14571550194e6c36dcb69f
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-09-05 21:43:35 +0200

    Update index.php

--- plugins/image_db/index.php
+++ plugins/image_db/index.php
@@ -1,1 +1,14 @@
-<?php // silence is golden
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    image_db/index.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ * silence is golden
+ */
