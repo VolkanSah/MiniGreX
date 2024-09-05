5723f4ae7575146b4f631ca1d7df6232d7177842
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-09-05 17:37:22 +0200

    Update loader.php

--- includes/loader.php
+++ includes/loader.php
@@ -14,7 +14,7 @@ define('CMS_MGREX', ROOT_PATH . 'includes/cms.php');
 
 // Cache files
 define('CACHE_MGREX', ROOT_PATH . 'cache/cache_db.php'); 
-define('CACHE_PH_MGREX', ROOT_PATH . 'cache/index.php'); // Placeholder cache file
+# define('CACHE_PH_MGREX', ROOT_PATH . 'cache/index.php'); // Placeholder cache file
 
 // Plugin loader
 define('PLUGIN_LOADER', ROOT_PATH . 'plugins/plugin_loader.php');
@@ -32,5 +32,33 @@ define('ADMIN_USER_SETTINGS', ROOT_PATH . 'panel/user_settings.php'); // Geände
 define('MGREX_INDEX', ROOT_PATH . 'index.php');
 define('MGREX_LOGIN', ROOT_PATH . 'login.php');
 define('MGREX_REGISTER', ROOT_PATH . 'register.php');
+
+// Include the init file
+require_once INIT_MGREX; // MiniGrex init
+require_once SECURITY_MGREX; // Security init
+require_once LOOP_MGREX; // Loop init
+require_once FUNCTION_MGREX; // Functions init
+require_once UPLOAD_MGREX; // Uploads init
+require_once IMAGES_MGREX; // Images init
+require_once CMS_MGREX; // CMS init
+
+// Cache files
+require_once CACHE_MGREX; // Cache init
+
+// Plugin and theme loaders
+require_once PLUGIN_LOADER; // Plugin init (integration of plugins)
+require_once THEME_LOADER; // Theme init (integration of themes)
+
+// Admin panel files
+require_once ADMIN_PANEL; // Admin panel init
+require_once ADMIN_DASH; // Dashboard init
+require_once ADMIN_OPTION; // Option settings init
+require_once ADMIN_USER_SETTINGS; // User settings init
+
+// Public files
+require_once MGREX_INDEX; // Main index file
+require_once MGREX_LOGIN; // Login file
+require_once MGREX_REGISTER; // Register file
+
 
 ?>
