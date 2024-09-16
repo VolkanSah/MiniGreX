a0cd8a9d886c7eb8fcef2b58962609f8d1390629
Volkan Şah <VolkanSah@users.noreply.github.com>
2024-09-16 22:49:53 +0200

    Update index.php

--- uploads/index.php
+++ uploads/index.php
@@ -1,1 +1,15 @@
-
+<?php
+/**
+ *
+ * MiniGreX - designed with security in mind!
+ *
+ * @file    uploads/index.php
+ * @package MiniGreX
+ * @copyright  Volkan Kücükbudak 
+ * @version 0.9 Beta
+ * @license MIT
+ * @link    https://github.com/VolkanSah/MiniGreX
+ *
+ * Upload-folder
+ * Do not remove this file! Silence is golden
+ */
